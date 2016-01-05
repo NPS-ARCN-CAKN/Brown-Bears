24525713b227ee040b96061e91f96889b00f1997
RedSkeeter <hexagenia@hotmail.com>
2016-01-05 11:21:44 -0600

    Batch file to quickly create bear monitoring field data deliverables directories

---
+++ CreateDeliverablesDirectories.bat
@@ -0,0 +1,11 @@
+REM Purpose: Batch file to quickly create bear monitoring field data deliverables directories
+REM How to use: Copy this batch file into the directory where you would like the subdirectories created. Ensure the file has a .bat extension. Double click to execute
+REM Written by SDMiller, NPS Arctic Network Inventory and Monitoring Program
+REM https://science.nature.nps.gov/im/units/arcn/vitalsign.cfm?vsid=18
+REM https://github.com/NPS-ARCN-CAKN/Brown-Bears
+md "Deliverable BB-01-Waypoints"
+md "Deliverable BB-02-Tracklogs"
+md "Deliverable BB-03-Photos"
+md "Deliverable BB-04-Scanned Datasheets"
+md "Deliverable BB-05-Survey Units"
+echo "Directories created"
