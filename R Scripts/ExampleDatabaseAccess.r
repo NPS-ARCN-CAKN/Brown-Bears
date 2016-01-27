028d6b4d7efc4917a92ea8c40bbbac33618b9d2f
RedSkeeter <hexagenia@hotmail.com>
2016-01-27 13:01:34 -0600

    Changed the example to point to the data summary view

--- R Scripts/ExampleDatabaseAccess.r
+++ R Scripts/ExampleDatabaseAccess.r
@@ -4,8 +4,25 @@ library(RODBC)
 # Create a connection object to the ODBC connection titled "ARCN Brown Bears"
 Connection <-odbcConnect("ARCN Brown Bears")
 # Retrieve the contents of the SurveyGroups table into a data frame
-SurveyGroups <- sqlFetch(Connection, "SurveyGroups")
+SurveyGroups <- sqlFetch(Connection, "SurveyGroups")B
 # Retrieve bear groups data into a data frame by querying view
-BearGroups <- sqlQuery(Connection, "SELECT TOP 3 *  FROM vwBearGroups;")
+BearGroups <- sqlQuery(Connection, "SELECT [SurveyGroupName]
+      ,[Unit]
+      ,[CountOfUnitFlights]
+      ,[GroupNumber]
+      ,[CountOfGroupSightings]
+      ,[FirstTailNo]
+      ,[SecondTailNo]
+      ,[FirstPlaneSaw]
+      ,[SecondPlaneSaw]
+      ,[BothPlanesSaw]
+      ,[FirstAdults]
+      ,[SecondAdults]
+      ,[FirstCubs]
+      ,[SecondCubs]
+      ,[FirstWPInUnit]
+      ,[SecondWPInUnit]
+  FROM [CompositionCountSurveys].[dbo].[vwBearSightability]
+  WHERE  CountOfUnitFlights = 2 And Not GroupNumber Is NULL")
 # Close the database connection
 close(Connection)
